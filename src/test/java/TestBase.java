b4f469bac6c379f203d64a62fdcb2c9368582a69
TMaksyutov <dadojkeee@mail.ru>
2022-11-26 14:12:10 +0300

    edit TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,10 +4,5 @@ public class TestBase {
             listUsers = "https://reqres.in/api/users?page=2",
             singleUser = "https://reqres.in/api/users/2",
             createUser = "https://reqres.in/api/users",
-
-    // register
-    register = "https://reqres.in/api/register",
-
-    // login
-    login = "https://reqres.in/api/login";
+            login = "https://reqres.in/api/login";
 }
