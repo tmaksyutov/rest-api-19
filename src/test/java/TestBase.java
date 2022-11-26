b8834d6fc733d1bf1a3a9c7673acabc3ffcaa29e
TMaksyutov <dadojkeee@mail.ru>
2022-11-26 13:34:50 +0300

    added post request tests

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,13 @@
+public class TestBase {
+    String
+            // users
+            listUsers = "https://reqres.in/api/users?page=2",
+            singleUser = "https://reqres.in/api/users/2",
+            createUser = "https://reqres.in/api/users",
+
+    // register
+    register = "https://reqres.in/api/register",
+
+    // login
+    login = "https://reqres.in/api/login";
+}
